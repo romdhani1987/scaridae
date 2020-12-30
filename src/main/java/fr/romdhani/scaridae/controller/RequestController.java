6d3c515d4b50addab5e54697680d589cfdcc422a
aromdhani <aromdhani@rs2d.com>
2020-12-30 23:48:37 +0100

    [scaridae] initial commit

--- src/main/java/fr/romdhani/scaridae/controller/RequestController.java
+++ src/main/java/fr/romdhani/scaridae/controller/RequestController.java
@@ -1,21 +1,42 @@
 package fr.romdhani.scaridae.controller;
 
 import fr.romdhani.scaridae.core.database.DBEntityManager;
-import fr.romdhani.scaridae.core.orm.RequestAccess;
+import javax.persistence.Query;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class RequestController {
 
-    public boolean newAccessRequest(RequestAccess requestAccess) {
+
+    public <T> boolean add(T newRequest) {
         try {
-            DBEntityManager.getInstance().persistEntities(requestAccess);
-            return true;
+            if (newRequest instanceof Serializable) {
+                Serializable request = (Serializable) newRequest;
+                DBEntityManager.getInstance().persistEntities(request);
+                return true;
+            } else {
+                return false;
+            }
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    public void getAccessRequest() {
-
+    public <T> List<T> getList(final String reqAsString) {
+        List<T> list = new ArrayList<>();
+        try {
+            DBEntityManager.getInstance().doInTransaction(em -> {
+                Query query = em.createQuery(reqAsString);
+                list.addAll(query.getResultList());
+            });
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return list;
+        }
     }
+
 }
